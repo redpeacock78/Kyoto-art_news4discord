8a7dd892139ceb719095cc50c05635090c854fee
redpeacock78 <1025708705@qq.com>
2020-02-09 12:38:27 +0900

    Change content: src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 function main() {
   const url = "https://kyotoartnews.page.link/all";
   const result: string[] = rss_perse(url);
-  const diff: string[] = diff_result(result).reverse();
+  const diff: string[] = diff_result(result).slice().reverse();
   const length: number = diff.length;
   if (length > 0) {
     discord(diff);
