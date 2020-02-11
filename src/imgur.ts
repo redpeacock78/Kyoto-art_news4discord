8939a27ec44e9889ff5a5b2cec3c5c11f0fff1cc
redpeacock78 <scarletpeacock78@gmail.com>
2020-02-11 10:03:27 +0900

    Change content: src/imgur.ts

---
+++ src/imgur.ts
@@ -0,0 +1,26 @@
+function upload_ogp(title) {
+  const id =
+    "Client-ID " +
+    PropertiesService.getScriptProperties().getProperty("client_id");
+  const ogp_url =
+    "https://res.cloudinary.com/dy7i2k9d4/image/upload/l_text:Sawarabi%20Gothic_45:" +
+    title +
+    ",w_800,c_fit/v1581149440/OGP/IMG_0172_qjc2qa.png";
+  const imgur_url = "https://api.imgur.com/3/image";
+
+  const resp = UrlFetchApp.fetch(ogp_url, { method: "get" });
+  const resp_blob = resp.getBlob();
+
+  const content: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: "post",
+    headers: {
+      Authorization: id
+    },
+    payload: resp_blob
+  };
+
+  const imgur_resp = UrlFetchApp.fetch(imgur_url, content).getContentText();
+  const imgur_json = JSON.parse(imgur_resp);
+
+  return imgur_json.data.link;
+}
