9fc30da7de941f4fd9fe1d3005f209e6aa4328d9
redpeacock78 <scarletpeacock78@gmail.com>
2020-02-13 13:24:32 +0900

    Change content: src/generate_description.ts

--- src/generate_description.ts
+++ src/generate_description.ts
@@ -4,6 +4,7 @@ function generate_description(URL) {
     "&gt;": ">",
     "&amp;": "&",
     "&quot;": '"',
+    "&apos;": "'",
     "&yen;": "¥",
     "&copy;": "©",
     "&brvbar;": "¦",
@@ -19,6 +20,7 @@ function generate_description(URL) {
   const character_num = {
     "&#038;": "&",
     "&#034;": '"',
+    "&#039;": "'",
     "&#165;": "¥",
     "&#169;": "©",
     "&#166;": "¦",
@@ -46,11 +48,11 @@ function generate_description(URL) {
     .replace(/^ +| +$|^&#032|&#032$|&nbsp;|&#160;|&#010;|&#009;|&#013/g, "")
     .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
     .replace(
-      /&(lt|gt|amp|quot|yen|copy|brvbar|reg|beg|plusmn|times|divide|acute|micro|middot);/g,
+      /&(lt|gt|amp|quot|apos|yen|copy|brvbar|reg|beg|plusmn|times|divide|acute|micro|middot);/g,
       c => character_name[c]
     )
     .replace(
-      /&#(034|038|165|166|169|174|176|177|215|247|180|181|183);/g,
+      /&#(034|038|039|165|166|169|174|176|177|215|247|180|181|183);/g,
       c => character_num[c]
     );
   const comp_text = text_block => {
