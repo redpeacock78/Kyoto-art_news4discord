85791ac1cc74ade9c283752b3a28c683ce4522da
redpeacock78 <scarletpeacock78@gmail.com>
2020-02-14 03:47:00 +0900

    Change content: src/generate_description.ts

--- src/generate_description.ts
+++ src/generate_description.ts
@@ -1,38 +1,6 @@
 function generate_description(URL) {
-  const character_name = {
-    "&lt;": "<",
-    "&gt;": ">",
-    "&amp;": "&",
-    "&quot;": '"',
-    "&apos;": "'",
-    "&yen;": "¥",
-    "&copy;": "©",
-    "&brvbar;": "¦",
-    "&reg;": "®",
-    "&beg;": "°",
-    "&plusmn;": "±",
-    "&times;": "×",
-    "&divide;": "÷",
-    "&acute;": "´",
-    "&micro;": "µ",
-    "&middot;": "·"
-  };
-  const character_num = {
-    "&#038;": "&",
-    "&#034;": '"',
-    "&#039;": "'",
-    "&#165;": "¥",
-    "&#169;": "©",
-    "&#166;": "¦",
-    "&#174;": "®",
-    "&#176;": "°",
-    "&#177;": "±",
-    "&#215;": "×",
-    "&#247;": "÷",
-    "&#180;": "´",
-    "&#181;": "µ",
-    "&#183;": "·"
-  };
+  //Underscore for GAS: M3i7wmUA_5n0NSEaa6NnNqOBao7QLBR4j
+  const _ = Underscore.load();
 
   const url_resp = UrlFetchApp.fetch(URL)
     .getContentText()
@@ -43,18 +11,12 @@ function generate_description(URL) {
   const last_num = url_resp.indexOf(
     '                              <div class="post-sub-block ve">'
   );
-  const text_block = String(url_resp.slice(start_num, last_num))
-    .replace(/,/g, "")
-    .replace(/^ +| +$|^&#032|&#032$|&nbsp;|&#160;|&#010;|&#009;|&#013/g, "")
-    .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
-    .replace(
-      /&(lt|gt|amp|quot|apos|yen|copy|brvbar|reg|beg|plusmn|times|divide|acute|micro|middot);/g,
-      c => character_name[c]
-    )
-    .replace(
-      /&#(034|038|039|165|166|169|174|176|177|215|247|180|181|183);/g,
-      c => character_num[c]
-    );
+  const text_block = _.unescape(
+    String(url_resp.slice(start_num, last_num))
+      .replace(/,/g, "")
+      .replace(/^ +| +$/g, "")
+      .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
+  );
   const comp_text = text_block => {
     if (text_block.length > 95) {
       return text_block.substr(0, 95) + "...";
