aeb202fb7b4b13dd51b2dc768da37a5e7409900e
redpeacock78 <scarletpeacock78@gmail.com>
2020-02-14 08:10:59 +0900

    Change content: src/rss_perse.ts

--- src/rss_perse.ts
+++ src/rss_perse.ts
@@ -9,7 +9,7 @@ function rss_perse(URL) {
   const length = items.length;
 
   const result = [];
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i = (i + 1) | 0) {
     const tit = items[i].getChildText("title");
     const link = items[i].getChildText("link");
     const desc = items[i].getChildText("description");
