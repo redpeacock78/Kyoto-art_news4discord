36ee95c05683e7b07498c4dbe969ad5c2112023b
redpeacock78 <scarletpeacock78@gmail.com>
2020-02-14 16:40:05 +0900

    Change content: src/generate_description.ts

--- src/generate_description.ts
+++ src/generate_description.ts
@@ -4,9 +4,18 @@ function generate_description(URL) {
 
   //URL先のHTMLをFetchして&nbsp;などを通常のスペースに置換した上で改行を基準に配列化
   const nbsp = String.fromCharCode(160);
+  const ensp = String.fromCharCode(8194);
+  const emsp = String.fromCharCode(8195);
+  const thinsp = String.fromCharCode(8201);
   const url_resp = UrlFetchApp.fetch(URL)
     .getContentText()
     .split(nbsp)
+    .join(" ")
+    .split(ensp)
+    .join(" ")
+    .split(emsp)
+    .join(" ")
+    .split(thinsp)
     .join(" ")
     .split(/\r\n|\r|\n/);
   //抜粋開始行数と終了行数の取得
