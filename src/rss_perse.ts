f042054c5716a15ece5631a439199500de22a7e9
redpeacock78 <scarletpeacock78@gmail.com>
2020-05-10 23:11:03 +0900

    Change content: src/rss_perse.ts

--- src/rss_perse.ts
+++ src/rss_perse.ts
@@ -1,18 +1,20 @@
-function rss_perse(URL) {
-  const resp = XmlService.parse(UrlFetchApp.fetch(URL).getContentText());
-  const xml = resp
+function rss_perse<T extends string>(URL: T): T[] {
+  const resp: GoogleAppsScript.XML_Service.Document = XmlService.parse(
+    UrlFetchApp.fetch(URL).getContentText()
+  );
+  const xml: GoogleAppsScript.XML_Service.Element[] = resp
     .getRootElement()
     .getChildren("channel")[0]
     .getChildren("item");
 
-  const items = xml;
-  const length = items.length;
+  const items: GoogleAppsScript.XML_Service.Element[] = xml;
+  const length: number = items.length;
 
   const result = [];
   for (let i = 0; i < length; i = (i + 1) | 0) {
-    const tit = items[i].getChildText("title");
-    const link = items[i].getChildText("link");
-    const desc = items[i].getChildText("description");
+    const tit: string = items[i].getChildText("title");
+    const link: string = items[i].getChildText("link");
+    const desc: string = items[i].getChildText("description");
     result[i] = [tit, link, desc];
   }
 
