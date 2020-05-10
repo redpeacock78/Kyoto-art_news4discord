72b29f6b0932494cb82439a3c77d80823823de56
redpeacock78 <scarletpeacock78@gmail.com>
2020-05-10 23:09:48 +0900

    Change content: src/discord.ts

--- src/discord.ts
+++ src/discord.ts
@@ -1,19 +1,21 @@
-function discord(message) {
-  const url = PropertiesService.getScriptProperties().getProperty("WEB_HOOK");
+function discord<T>(message: T[]): void {
+  const url: string = PropertiesService.getScriptProperties().getProperty(
+    "WEB_HOOK"
+  );
 
   for (let i = 0; i < message.length; i = (i + 1) | 0) {
     //テキストを生成
-    const description = generate_description(message[i][1]);
+    const description: string = generate_description<string>(message[i][1]);
 
     //OGP画像生成
-    const ogp_url = imgur(
+    const ogp_url: string = imgur<string>(
       encodeURIComponent(
         message[i][0].replace(/\,/g, "%2C").replace(/\//g, "%2F")
       )
     );
 
     //author_iconを設定
-    const icon_url = message[i][2]
+    const icon_url: string = message[i][2]
       .replace(/<img src="/, "")
       .replace(/" referrerpolicy="no-referrer">/, "");
 
@@ -35,7 +37,7 @@ function discord(message) {
     };
 
     //json本体をJSON形式に変換
-    const payload = JSON.stringify(json);
+    const payload: string = JSON.stringify(json);
 
     //変換したJSONをヘッダー情報に格納
     const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
