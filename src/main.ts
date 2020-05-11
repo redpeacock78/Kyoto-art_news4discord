773a1f8f9e84a536fbf7d501dee94269ca768fc7
redpeacock78 <scarletpeacock78@gmail.com>
2020-05-12 00:39:57 +0900

    Change Content: src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ This software is released under the MIT License, see LICENSE.
 function main(): void {
   const url = "https://kyotoartnews.page.link/all";
   const result = rss_perse<string>(url);
-  const diff = diff_result<string>(result)
+  const diff = diff_result<string[]>(result)
     .slice()
     .reverse();
-  diff.length > 0 && discord<string>(diff);
+  diff.length > 0 && discord<string[]>(diff);
 }
