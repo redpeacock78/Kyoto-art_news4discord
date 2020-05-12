db0cf3d191121516ade5a2e84b83c4e8fbc41dca
redpeacock78 <scarletpeacock78@gmail.com>
2020-05-13 00:12:24 +0900

    Change content: src/generate_description.ts

--- src/generate_description.ts
+++ src/generate_description.ts
@@ -8,29 +8,26 @@ function generate_description<T extends string>(URL: T): T {
   const start_num: number = url_resp.indexOf(
     '                              <div class="post-main-block ve">'
   );
-  const last_num: number = url_resp.indexOf(
+  const middle_num: number = url_resp.indexOf(
     '                              <div class="post-sub-block ve">'
   );
-  const start_num2: number = url_resp.indexOf(
-    '                              <div class="post-sub-block ve">'
-  );
-  const last_num2: number = url_resp.indexOf("      </main>");
+  const last_num: number = url_resp.indexOf("      </main>");
 
   //HTMLの抜粋とHTMLタグの除去及びHTMLエンティティのアンエスケープ処理、冒頭・文末の連続スペースの除去・連続スペースの統合
   const html_tag = new RegExp(/<("[^"]*"|'[^']*'|[^'">])*>/g);
-  const ext: string = url_resp
-    .slice(start_num, last_num)
+  const main_text: string = url_resp
+    .slice(start_num, middle_num)
     .join("")
     .replace(html_tag, "");
-  const ext2: string = url_resp
-    .slice(start_num2, last_num2)
+  const sub_text: string = url_resp
+    .slice(middle_num, last_num)
     .join("")
     .replace(html_tag, "");
   const texts_block: string = XmlService.parse(
     `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
       <d>
-       ${ext}
-       ${ext2}
+       ${main_text}
+       ${sub_text}
       </d>`
   )
     .getRootElement()
