59a806db62f4868f5773974193bcbcde71a7b7cd
redpeacock78 <scarletpeacock78@gmail.com>
2020-06-09 21:41:52 +0900

    Change content: src/generate_description.ts

--- src/generate_description.ts
+++ src/generate_description.ts
@@ -1,31 +1,40 @@
 async function generate_description<T extends string>(URL: T): Promise<T> {
   //URL先のHTMLをFetchして改行を基準に配列化
-  const url_resp = async (url: string): Promise<string[]> => {
+  const url_resp = async ({ url }: { url: string }): Promise<string[]> => {
     return UrlFetchApp.fetch(url)
       .getContentText()
       .split(/\r\n|\r|\n/);
   };
 
   //抜粋開始行数と終了行数の取得
-  const resp_array: string[] = await url_resp(URL);
-  const get_line_number = (search_word: string): number => {
+  const resp_array: string[] = await url_resp({ url: URL });
+  const get_line_number = ({
+    search_word
+  }: {
+    search_word: string;
+  }): number => {
     return resp_array.indexOf(search_word);
   };
-  const start: number = get_line_number(
-    '                              <div class="post-main-block ve">'
-  );
-  const middle: number = get_line_number(
-    '                              <div class="post-sub-block ve">'
-  );
-  const last: number = get_line_number("      </main>");
+  const start: number = get_line_number({
+    search_word:
+      '                              <div class="post-main-block ve">'
+  });
+  const middle: number = get_line_number({
+    search_word: '                              <div class="post-sub-block ve">'
+  });
+  const last: number = get_line_number({ search_word: "      </main>" });
 
   //HTMLの抜粋とHTMLタグの除去及びHTMLエンティティのアンエスケープ処理、冒頭・文末の連続スペースの除去・連続スペースの統合
   const html_tag = new RegExp(/<("[^"]*"|'[^']*'|[^'">])*>/g);
-  const html_excerpt_tag_removal = (
-    start: number,
-    end: number,
-    tag: RegExp
-  ): string => {
+  const html_excerpt_tag_removal = ({
+    start,
+    end,
+    tag
+  }: {
+    start: number;
+    end: number;
+    tag: RegExp;
+  }): string => {
     return resp_array
       .slice(start, end)
       .join("")
@@ -34,8 +43,8 @@ async function generate_description<T extends string>(URL: T): Promise<T> {
   const texts_block: string = XmlService.parse(
     `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
       <d>
-       ${html_excerpt_tag_removal(start, middle, html_tag)}
-       ${html_excerpt_tag_removal(middle, last, html_tag)}
+       ${html_excerpt_tag_removal({ start: start, end: middle, tag: html_tag })}
+       ${html_excerpt_tag_removal({ start: middle, end: last, tag: html_tag })}
       </d>`
   )
     .getRootElement()
